06ab04f45f68cd38cc381ce77c56c2df0bb06bae
rhuarhri <rhuarhri@gmail.com>
2018-11-06 14:19:29 +0000

    the start

---
+++ app/src/main/java/com/example/rhuarhri/carmaintenancechatbot/keyWordSearch.java
@@ -0,0 +1,15 @@
+package com.example.rhuarhri.carmaintenancechatbot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class keyWordSearch {
+
+    public List<String> search(String userResponse)
+    {
+        List Result = new ArrayList<String>();
+
+
+        return Result;
+    }
+}
--- app/src/test/java/com/example/rhuarhri/carmaintenancechatbot/ExampleUnitTest.java
+++ app/src/test/java/com/example/rhuarhri/carmaintenancechatbot/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package com.example.rhuarhri.carmaintenancechatbot;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +16,21 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void findKeyWords()
+    {
+        String input = "What is a gear box?";
+
+        keyWordSearch test = new keyWordSearch();
+        List result = test.search(input);
+
+        List<String> expected = new ArrayList<String>();
+        expected.add("What");
+        expected.add("gear");
+        expected.add("box");
+
+        assertArrayEquals(expected.toArray(), result.toArray());
     }
 }
